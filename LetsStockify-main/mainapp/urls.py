25cca52183c90560f6099d0910686078ed42409e
kashimmirza <kashimmirza86@gmail.com>
2023-02-09 00:52:16 +0600

    error has been fixed

--- LetsStockify-main/mainapp/urls.py
+++ LetsStockify-main/mainapp/urls.py
@@ -2,6 +2,6 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.stockPickers, name='stockpicker'),
+    path('', views.stockPicker, name='stockpicker'),
     path('stocktracker/', views.stockTracker, name='stocktracker'),
 ]
