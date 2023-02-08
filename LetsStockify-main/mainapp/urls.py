b32d1da307a10e9674ca02b339de3338485ef3d7
kashimmirza <kashimmirza86@gmail.com>
2023-02-09 00:50:45 +0600

    this has been fixed

--- LetsStockify-main/mainapp/urls.py
+++ LetsStockify-main/mainapp/urls.py
@@ -2,6 +2,6 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.stockPicker, name = 'stockpicker'),
-    path('stocktracker/', views.stockTracker, name = 'stocktracker'),
+    path('', views.stockPickers, name='stockpicker'),
+    path('stocktracker/', views.stockTracker, name='stocktracker'),
 ]
